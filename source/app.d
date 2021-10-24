e253fd939c81495fb09934d284873c918c50c18f
Selim Ozel <sozel@wpi.edu>
2021-10-24 12:37:34 +0200

    trying to do some chunks in zip

--- source/app.d
+++ source/app.d
@@ -1,7 +1,7 @@
 import std.csv: csvReader;
 import std.mmfile;
 import std.stdio;
-import std.zip;
+import selim.zip;
 
 struct quandl_row_layout {
 	string symbol;
@@ -26,16 +26,23 @@ void main()
 	auto mmfile = new MmFile("EOD.complete.zip");
 	auto zipData = new ZipArchive(mmfile[]);
 	string csvPath_to = "";
+
+	ulong number_of_lines = 0;
+
 	foreach (name, am; zipData.directory) {
 		string rawtext = cast(string)zipData.expand(am);
-		auto records = rawtext.csvReader!quandl_row_layout(',');
+		//auto records = rawtext.csvReader!quandl_row_layout(',');
 		// abcdefghijklmnopqrstuvyzx
 		// B works
 		// L does not work. Row length does not match.
 		// D Row 4798184's length 13 does not match previous length of 14.
+		/*
 		foreach (record; records) {
 			if(record.symbol[0] == 'D') writeln(record);
+			number_of_lines++;
+			if(number_of_lines % 500000 == 0) writeln(record);			
 		}
+		*/
 		destroy(mmfile);
 	}
 }
