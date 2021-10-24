edf92ea9f610a2210158a7b95fff77d58f95d14f
Selim Ozel <sozel@wpi.edu>
2021-10-24 13:10:17 +0200

    changed perspective

--- source/app.d
+++ source/app.d
@@ -1,20 +1,6 @@
-import std.csv: csvReader;
-import std.mmfile;
 import std.stdio;
-import selim.zip;
-import std.zlib;
-import std.conv : to;
-import std.algorithm.iteration : map;
 
 void main()
 {
-	import std.file: write;
-	auto mmfile = new MmFile("EOD.partial.zip");
-	auto zipData = new ZipArchive(mmfile[]);
-
-	foreach (name, am; zipData.directory) {
-		string rawtext = cast(string)zipData.expand(am);
-		write("EOD.partial.csv", rawtext);
-		destroy(mmfile);
-	}
+	writeln("7zip caller");
 }
