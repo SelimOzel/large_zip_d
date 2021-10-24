4f0ef4ff6ed24226ba1abcc990571033bd16ef81
Selim Ozel <sozel@wpi.edu>
2021-10-24 12:52:00 +0200

    Works fine on subset of big data using the forked zip library

--- source/app.d
+++ source/app.d
@@ -2,9 +2,19 @@ import std.csv: csvReader;
 import std.mmfile;
 import std.stdio;
 import selim.zip;
-
+import std.zlib;
+import std.conv : to;
+import std.algorithm.iteration : map;
 
 void main()
 {
+	import std.file: write;
+	auto mmfile = new MmFile("EOD.partial.zip");
+	auto zipData = new ZipArchive(mmfile[]);
 
+	foreach (name, am; zipData.directory) {
+		string rawtext = cast(string)zipData.expand(am);
+		write("EOD.partial.csv", rawtext);
+		destroy(mmfile);
+	}
 }
