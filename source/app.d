a8fa03a0c00f04fa4a64473ebc5b7d436bdb8399
Selim Ozel <sozel@wpi.edu>
2021-10-24 09:55:37 +0200

    Breaks down at a ridiculous line.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,24 @@
+import std.csv: csvReader;
 import std.mmfile;
 import std.stdio;
 import std.zip;
+
+struct quandl_row_layout {
+	string symbol;
+	string date;
+	string open;
+	string high;
+	string low;
+	string close;
+	string volume;
+	string dividend;
+	string split;
+	string adj_open;
+	string adj_high;
+	string adj_low;
+	string adj_close;
+	string adj_volume;
+}
 
 void main()
 {
@@ -9,7 +27,15 @@ void main()
 	auto zipData = new ZipArchive(mmfile[]);
 	string csvPath_to = "";
 	foreach (name, am; zipData.directory) {
-		write(name, zipData.expand(am));
+		string rawtext = cast(string)zipData.expand(am);
+		auto records = rawtext.csvReader!quandl_row_layout(',');
+		// abcdefghijklmnopqrstuvyzx
+		// B works
+		// L does not work. Row length does not match.
+		// D Row 4798184's length 13 does not match previous length of 14.
+		foreach (record; records) {
+			if(record.symbol[0] == 'D') writeln(record);
+		}
 		destroy(mmfile);
 	}
 }
