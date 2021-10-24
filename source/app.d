84b43bb1521639772ac7039c2023983781e9d8c9
Selim Ozel <sozel@wpi.edu>
2021-10-24 13:56:43 +0200

    Call 7z from dlang.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,11 @@
+import std.process;
 import std.stdio;
 
 void main()
 {
-	writeln("7zip caller");
+	string path_7z = "\"C:\\Program Files\\7-Zip\\\"7z";
+	string command_7z = " e ";
+	string zip_file_destination = "\"C:\\Software\\large_zip_d\\EOD.complete.zip\"";
+	auto result = executeShell(path_7z~command_7z~zip_file_destination);
+	writeln(result.output);
 }
